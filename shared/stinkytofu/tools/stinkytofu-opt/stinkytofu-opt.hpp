7feab3fd7f923dd1d0a2f7e22b7b646f29cbbf26
johnny19436 <johnny194369672@gmail.com>
2026-07-22 06:48:07 +0000

    feat(stinkytofu): add AsmMovePropagationPass

--- shared/stinkytofu/tools/stinkytofu-opt/stinkytofu-opt.hpp
+++ shared/stinkytofu/tools/stinkytofu-opt/stinkytofu-opt.hpp
@@ -47,6 +47,7 @@
 #include "stinkytofu/transforms/asm/MemTokenConsistencyCheckPass.hpp"
 #include "stinkytofu/transforms/asm/PeepholeOptimizationPass.hpp"
 #include "stinkytofu/transforms/asm/RaiseVgprMsbPass.hpp"
+#include "stinkytofu/transforms/asm/AsmMovePropagationPass.hpp"
 #include "stinkytofu/transforms/asm/RedundantMovEliminationPass.hpp"
 #include "stinkytofu/transforms/asm/RegionClonePass.hpp"
 #include "stinkytofu/transforms/asm/RemoveDelayAluPass.hpp"
@@ -128,6 +129,7 @@ const std::vector<PassInfo> availablePasses = {
     {"DeadCodeEliminationPass", [](const auto&) { return createDeadCodeEliminationPass(); }},
     {"RedundantMovEliminationPass",
      [](const auto&) { return createRedundantMovEliminationPass(); }},
+    {"AsmMovePropagationPass", [](const auto&) { return createAsmMovePropagationPass(); }},
     {"StinkyIRVerifierPass", [](const auto&) { return createStinkyIRVerifierPass(); }},
     {"RemoveDelayAluPass", [](const auto&) { return createRemoveDelayAluPass(); }},
     // RemoveInstructionPass accepts one or more mnemonics:
