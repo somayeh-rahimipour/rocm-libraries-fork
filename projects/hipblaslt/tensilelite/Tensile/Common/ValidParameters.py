d10bf8014fdfc5bc54e40858df418e7a1c16e982
boringmorning <huangchen1999@gmail.com>
2026-08-14 09:08:38 +0000

    amend comments

--- projects/hipblaslt/tensilelite/Tensile/Common/ValidParameters.py
+++ projects/hipblaslt/tensilelite/Tensile/Common/ValidParameters.py
@@ -354,10 +354,11 @@ validParameters = { # we need to make sure this matches develop
     # 0: disable
     # 1: prefetch one load tile (MTxDepthU) ahead of PrefetchGlobalRead
     # 2: prefetch two load tiles (MTxDepthU) ahead of PrefetchGlobalRead
-    # Currently we do not support StaggerU (forced off), general batch, or Stream-K
-    # other than DP-first (StreamK==3). GSU is supported, but only at the value the
-    # solution was tuned with (SupportUserGSU is forced off, so it cannot be
-    # overridden at runtime). May remove these limitations in the future.
+    # Currently we do not support general batch, or Stream-K other than DP-first
+    # (StreamK==3). GSU is supported, but only at the value the solution was tuned
+    # with (SupportUserGSU is forced off, so it cannot be overridden at runtime).
+    # StaggerU is supported, except under PrefetchAcrossPersistent where it is
+    # forced off. May remove these limitations in the future.
     "PrefetchGL2": [0, 1, 2],
     # MatrixInstruction Only
     # If set ClusterLocalRead, each iteration dedicated vgprBuffer for localRead
