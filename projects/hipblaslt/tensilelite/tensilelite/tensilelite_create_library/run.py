6fec6a034b97a1877f9653e184113b6f3d124df4
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2026-08-18 18:13:37 +0000

    3-refactor(tensilelite): lower-case library creation and logic packages

--- projects/hipblaslt/tensilelite/tensilelite/tensilelite_create_library/run.py
+++ projects/hipblaslt/tensilelite/tensilelite/tensilelite_create_library/run.py
@@ -84,7 +84,7 @@ from tensilelite.Toolchain.Component import Assembler
 from tensilelite.Utilities.Decorators.Profile import profile
 from tensilelite.Utilities.Decorators.Timing import timing
 from ..resources import copy_static_headers
-from .ParseArguments import parseArguments
+from .parse_arguments import parseArguments
 
 
 def libraryRoot(outputPath: Union[str, Path]) -> Path:
